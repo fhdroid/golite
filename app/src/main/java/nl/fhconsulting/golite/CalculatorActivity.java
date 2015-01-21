307a73e759d4f57a04ecda34c749bb7cbc378333
fhdroid <fhdroid@gmail.com>
2015-01-21 17:00:16 +0100

    added substraction and multiplication

--- app/src/main/java/nl/fhconsulting/golite/CalculatorActivity.java
+++ app/src/main/java/nl/fhconsulting/golite/CalculatorActivity.java
@@ -20,6 +20,7 @@ package nl.fhconsulting.golite;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -33,6 +34,9 @@ public class CalculatorActivity extends Activity {
     Button level2;
     Button level3;
 
+    private static final int PLUS  = 1;
+    private static final int MINUS = 2;
+    private static final int TIMES = 3;
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
@@ -76,43 +80,78 @@ public class CalculatorActivity extends Activity {
     private void setUp(int level) {
         Random r = new Random();
         int factor = 10;
+        final int operator;
+        final EditText input = (EditText)findViewById(R.id.inputField);
         switch (level) {
             case 1:
                 factor=10;
                 level1.setBackground(getResources().getDrawable(R.drawable.roundedbutton_green) );
                 level2.setBackground(getResources().getDrawable(R.drawable.roundedbutton_red) );
                 level3.setBackground(getResources().getDrawable(R.drawable.roundedbutton_red) );
+                operator = PLUS; //simple +
+                //max input
+                input.setFilters(new InputFilter[] {new InputFilter.LengthFilter(2)});
                 break;
             case 2:
                 factor=100;
                 level1.setBackground(getResources().getDrawable(R.drawable.roundedbutton_red) );
                 level2.setBackground(getResources().getDrawable(R.drawable.roundedbutton_green) );
                 level3.setBackground(getResources().getDrawable(R.drawable.roundedbutton_red) );
+                operator = r.nextInt(2) + 1; // + or -
+                input.setFilters(new InputFilter[] {new InputFilter.LengthFilter(5)});
                 break;
             case 3:
                 factor=500;
                 level1.setBackground(getResources().getDrawable(R.drawable.roundedbutton_red) );
                 level2.setBackground(getResources().getDrawable(R.drawable.roundedbutton_red) );
                 level3.setBackground(getResources().getDrawable(R.drawable.roundedbutton_green) );
+                operator = r.nextInt(3) + 1; //+, - and *
+                input.setFilters(new InputFilter[] {new InputFilter.LengthFilter(6)});
                 break;
             default:
                 factor=10;
                 level1.setBackground(getResources().getDrawable(R.drawable.roundedbutton_green) );
                 level2.setBackground(getResources().getDrawable(R.drawable.roundedbutton_red) );
                 level3.setBackground(getResources().getDrawable(R.drawable.roundedbutton_red) );
+                input.setFilters(new InputFilter[] {new InputFilter.LengthFilter(2)});
+                operator = PLUS;
         }
         final int firstValue  = r.nextInt(factor - 2) + 1;
-        final int secondValue = r.nextInt(factor - 2) + 1;
+        int secondValueTemp = r.nextInt(factor - 2) + 1;
+        if ( operator == MINUS && (secondValueTemp >= firstValue) ) {
+            do {
+                secondValueTemp = r.nextInt(factor - 2) + 1;
+            }
+            while (secondValueTemp >= firstValue);
+        }
+        final int secondValue = secondValueTemp;
         TextView firstValueText = (TextView)findViewById(R.id.firstValue);
         firstValueText.setText(Integer.toString(firstValue));
         TextView secondValueText = (TextView)findViewById(R.id.secondValue);
         secondValueText.setText(Integer.toString(secondValue));
+        TextView operatorText = (TextView)findViewById(R.id.operator);
+        switch (operator) {
+            case PLUS:
+                operatorText.setText("+");
+            break;
+            case MINUS:
+                operatorText.setText("-");
+            break;
+            case TIMES:
+                operatorText.setText("x");
+            break;
+            default:
+                operatorText.setText("+");
+            break;
 
-        final EditText input = (EditText)findViewById(R.id.inputField);
+        }
+
+
         Button checkCalcButton = (Button)findViewById(R.id.checkCalcButton);
         checkCalcButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-               if ( (!input.getText().toString().equals("")) && (firstValue + secondValue) ==  Integer.parseInt(input.getText().toString())) {
+               //if ( (!input.getText().toString().equals("")) && (firstValue + secondValue) ==  Integer.parseInt(input.getText().toString())) {
+                if (!input.getText().toString().equals("") && (checkSum(firstValue, secondValue, operator, Integer.parseInt(input.getText().toString())))) {
                    CommandStrings.playSound(CommandStrings.soundOhYeah);
                    againButton.setVisibility(View.VISIBLE);
                    againButton.setOnClickListener(new View.OnClickListener() {
@@ -126,6 +165,29 @@ public class CalculatorActivity extends Activity {
                }
             }
         });
+    }
+
+    private boolean checkSum(int i, int j, int operator, int answer) {
+        boolean bool = false;
+        switch(operator) {
+            case PLUS:
+                if ( i + j == answer) {
+                    bool = true;
+                }
+                break;
+            case MINUS:
+                if ( i - j == answer) {
+                    bool = true;
+                }
+                break;
+            case TIMES:
+                if ( i * j == answer) {
+                    bool = true;
+                }
+                break;
+
+        }
+        return bool;
     }
 
 }
